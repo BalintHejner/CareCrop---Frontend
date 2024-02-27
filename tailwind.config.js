b46b6e33dc939f83ae7919777ee220760ca3f0a3
BalintHejner <balinthejner204@gmail.com>
2024-02-27 11:32:52 +0100

    🧑‍💻 LoginPage design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'body': "#72A06A",
+      'header': "#416422"
+    },
     extend: {},
   },
   plugins: [],
