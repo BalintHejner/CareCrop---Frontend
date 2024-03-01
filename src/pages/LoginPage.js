eefeb23ece51cd90866c521d068e322d917c8efa
BalintHejner <balinthejner204@gmail.com>
2024-03-01 09:49:36 +0100

    Footer section

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -21,18 +21,26 @@ function LoginForm() {
       <div className="mt-7 text-3xl font-semibold">Jelszó:</div>
       <input className="shadow appearance-none border mt-3 border-red-500 h-input rounded w-360 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Jelszó"/>
       <Button/>
-      <div className="flex gap-5 justify-between mt-20 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-      <div className="flex flex-col flex-1 px-5 text-center">
-        <div className="text-4xl">Nincs még fiókja?</div>
-        <div className="self-center mt-4 text-3xl italic font-extralight underline" onClick={console.log("REGISZTRÁLJ")}>Regisztráljon</div>
-      </div>
-      <div className="flex flex-col flex-1 px-5 my-auto">
-        <div className="text-4xl text-center">Elfelejtette jelszavát?</div>
-        <div className="self-center mt-4 text-3xl italic font-extralight underline" onClick={""}>Jelszóemlékeztető</div>
-      </div>
-    </div>
+
     </>
   );
+}
+
+function Footer() {
+  return (
+    <>
+      <div className="flex gap-5 justify-between mt-20 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+        <div className="flex flex-col flex-1 px-5 text-center">
+          <div className="text-4xl">Nincs még fiókja?</div>
+          <div className="self-center mt-4 text-3xl italic font-extralight underline" onClick={console.log("REGISZTRÁLJ")}>Regisztráljon</div>
+        </div>
+      <div className="flex flex-col flex-1 px-5 my-auto">
+          <div className="text-4xl text-center">Elfelejtette jelszavát?</div>
+          <div className="self-center mt-4 text-3xl italic font-extralight underline" onClick={console.log("JELSZÓ")}>Jelszóemlékeztető</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 
@@ -41,6 +49,7 @@ function LoginPage() {
     <div className="flex flex-col items-center pb-12 h-max text-black whitespace-nowrap bg-body leading-[100%]">
       <Header />
       <LoginForm />
+      <Footer />
     </div>
   );
 }
