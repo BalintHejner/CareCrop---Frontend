895e437d146c11787bd54f4f854f2dcb7c5014e8
BalintHejner <balinthejner204@gmail.com>
2024-03-07 10:09:38 +0100

    Support page done

--- src/components/HeaderOption2.js
+++ src/components/HeaderOption2.js
@@ -1,22 +1,31 @@
 import "../output.css";
-
+ 
 export default function Header2() {
     return (
-        <div className="flex gap-5 justify-between self-stretch px-6 py-2 w-full text-2xl bg-header max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
-          <img loading="lazy" src={require('../images/carecroplogo.png')} className=" carecroplogo  " />
-          <select className="shadow appearance-none border rounded w-10px  py-2 px-3 text-gray-700 leading-tight">
-            <option>Termény</option>
-          </select>
+      <>
+
+        
+      <div className="flex  justify-between self-stretch px-6 py-2 w-full text-2xl bg-header max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
+        <img loading="lazy" src={require('../images/carecroplogo.png')} className=" carecroplogo" />
+        <div className="flex gap-2.5 justify-between px-3 py-2 h-input bg-white rounded border-2 border-blue-400 border-solid max-md:flex-wrap max-md:max-w-full">
+        <select className="shadow appearance-none border rounded  w-360 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <option value="" disabled selected>Válasszon árucsoportot</option>
+          <option value="Termény">Termény</option>
+          <option value="Terményvédő szerek" >Terményvédő szerek</option>
+        </select>
+        </div>
+
           <div className="flex gap-5 justify-between my-auto max-md:flex-wrap max-md:max-w-full">
             <div className="flex flex-col flex-1 justify-center my-auto max-md:max-w-full">
               <div className="flex gap-2.5 justify-between px-3 py-2 bg-white rounded border-2 border-blue-400 border-solid max-md:flex-wrap max-md:max-w-full">
-              <input className="shadow appearance-none border rounded w-10px  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Keresés"/>
+                <input className="shadow appearance-none border rounded w-10px  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Keresés"/>
                 <img loading="lazy" src={require('../images/searchLogo.png')} className="w-8 glass aspect-square" />
               </div>
             </div>
-            <img loading="lazy" src={require('../images/shopping-cart.jpg')} className="cart"/>
+            <img loading="lazy" src={require('../images/shopping-cart.png')} className="cart"/>
             <img loading="lazy" src={require('../images/loginMan.png')} className="aspect-square w-[82px]" />
           </div>
         </div>
+      </>
       );
 }
