68fd17efa87d155e9c9fb1d8ffb46bb71aa4abd3
BalintHejner <balinthejner204@gmail.com>
2024-03-11 10:23:44 +0100

    Small changes

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,40 +5,14 @@ import Button2 from "../components/Button2";
 import Input from "../components/Input";
 import { Dropdown } from 'primereact/dropdown';
 
-const ProductInfo = ({crop, amount, price, payment}) => (
-
-  <aside className="flex flex-col w-[33%] md:ml-0 md:w-full">
-  <div className="flex flex-col mt-2 text-base aspect-square text-black bg-brown md:mt-10 md:max-w-full">
-     <h1 className="ml-1 mg-5 tracking-[3.2px] font-bold md:max-w-full text-3xl">A kosár tartalma:</h1>
-     <hr className="w-500 bg-zinc-300 mg-5" />
-     <div className="flex flex-col w-full max-w-full">
-       <ul className="flex flex-col w-full max-w-full">
-         <li className="flex flex-col w-full max-w-full mg-5">{crop}
-         <ul>
-         <li>Mennyiség: {amount} kg</li>
-         <li>Ár: {price} Ft</li>
-         </ul>
-         </li>
-
-       </ul>
-   </div>
-
-   <hr className="w-500 bg-zinc-300 mg-5" />
-   <h1 className="ml-1 mg-5 tracking-[3.2px] font-bold md:max-w-full text-3xl">Fizetendő:</h1>
-   <hr className="w-500 bg-zinc-300 mg-5" />
-   <h1 className="ml-1 mr-50 tracking-[3.2px] font-bold md:max-w-full text-3xl text-right">{payment} Ft</h1>
-
-  </div>
- </aside>
-);
 
 const CartPage = () => {
   return (
     <main className="flex min-h-screen mx-auto min-w-screen flex-col pb-12 bg-body">
         <Header />
       <section className="self-center mt-16 w-full max-w-[1728px] md:mt-10 md:max-w-full">
-        <div className="flex gap-5 md:flex-col">
-          <aside className="flex flex-col w-[33%] md:ml-0 md:w-full">
+        <div className="flex gap-5 md:flex-col md:gap-0">
+          <aside className="flex flex-col w-[33%]  md:ml-0 md:w-full">
             <div className="flex flex-col mt-2 text-base text-black md:mt-10 md:max-w-full">
               <h2 className="  text-upper tracking-[3.2px] md:max-w-full">Teljes név:</h2>
               <Input placeholder={"Text field data"} type={"text"} />
@@ -64,5 +38,32 @@ const CartPage = () => {
     </main>
   );
 };
+
+const ProductInfo = ({crop, amount, price, payment}) => (
+
+  <aside className="flex flex-col w-[33%] md:ml-0 mx-auto md:w-full">
+  <div className="flex flex-col mt-2 text-base aspect-square mx-auto text-black bg-brown md:mt-10 md:max-w-full">
+     <h1 className="ml-1 mg-5 tracking-[3.2px] font-bold md:max-w-full text-3xl">A kosár tartalma:</h1>
+     <hr className="w-500 bg-zinc-300 mg-5" />
+     <div className="flex flex-col w-full max-w-full">
+       <ul className="flex flex-col w-full max-w-full">
+         <li className="flex flex-col w-full max-w-full mg-5">{crop}
+         <ul>
+         <li>Mennyiség: {amount} kg</li>
+         <li>Ár: {price} Ft</li>
+         </ul>
+         </li>
+
+       </ul>
+   </div>
+
+   <hr className="w-500 bg-zinc-300 mg-5" />
+   <h1 className="ml-1 mg-5 tracking-[3.2px] font-bold md:max-w-full text-3xl">Fizetendő:</h1>
+   <hr className="w-500 bg-zinc-300 mg-5" />
+   <h1 className="ml-1 mr-50 tracking-[3.2px] font-bold md:max-w-full text-3xl text-right">{payment} Ft</h1>
+
+  </div>
+ </aside>
+);
 
 export default CartPage;
